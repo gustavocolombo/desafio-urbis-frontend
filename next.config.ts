9d37df908df7f2db49ec3840869d0070b9afbd97
Gustavo Colombo <gusttavocolombo@gmail.com>
2025-03-20 17:23:21 -0300

    test: trying to deploy netlify

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,11 @@ import type { NextConfig } from "next";
 const API_URL = process.env.NEXT_PUBLIC_API_URL as string;
 
 const nextConfig: NextConfig = {
-
+  output: "export",
+  images: {
+    unoptimized: true, 
+  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
