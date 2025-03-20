9fb35ecc5d0ee0a1664d9134c40d483db2499faa
Gustavo Colombo <gusttavocolombo@gmail.com>
2025-03-20 17:01:08 -0300

    fix: rollback to static page

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,7 @@ import type { NextConfig } from "next";
 const API_URL = process.env.NEXT_PUBLIC_API_URL as string;
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`
-      },
-    ]
-  }
+
 };
 
 export default nextConfig;
